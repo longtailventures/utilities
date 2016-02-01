91a357288250fe2df397794f5aef18b5dfee5282
longtailventures <mnavasca@lotlinx.com>
2016-02-01 16:25:49 -0600

    Update PdoDebug.php

--- src/PdoDebug.php
+++ src/PdoDebug.php
@@ -1,1 +1,18 @@
 <?php
+
+namespace LongTailVentures;
+
+class PdoDebug
+{
+    public static function getPreparedQuery($query, array $queryParams)
+    {
+        $preparedQuery = $query;
+
+        krsort($queryParams);
+
+        foreach ($queryParams as $column => $value)
+            $preparedQuery = str_replace($column, "'$value'", $preparedQuery);
+
+        return $preparedQuery;
+    }
+}
