9ceee5576121c8a1c11e1d6183a03d2b6cd02967
longtailventures <mnavasca@lotlinx.com>
2017-05-31 09:30:41 -0500

    added html formating

--- src/PdoDebug.php
+++ src/PdoDebug.php
@@ -4,7 +4,7 @@ namespace LongTailVentures;
 
 class PdoDebug
 {
-    public static function getPreparedQuery($query, array $queryParams)
+    public static function getPreparedQuery($query, array $queryParams, $returnFormat = 'text')
     {
         $preparedQuery = $query;
 
@@ -12,6 +12,12 @@ class PdoDebug
 
         foreach ($queryParams as $column => $value)
             $preparedQuery = str_replace($column, "'$value'", $preparedQuery);
+
+        if ($returnFormat === 'html')
+        {
+            $preparedQuery = str_replace([PHP_EOL, "\t"], ['<br />', '&nbsp;&nbsp&nbsp;&nbsp'], $preparedQuery);
+            $preparedQuery = "<pre>$preparedQuery</pre>";
+        }
 
         return $preparedQuery;
     }
